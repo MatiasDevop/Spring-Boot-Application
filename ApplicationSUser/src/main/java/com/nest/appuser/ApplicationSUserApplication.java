f3adbcee5c703d5a8ea68afd753ba100f3e047aa
nestor m <solomatias321@gmail.com>
2019-12-12 17:13:04 +0100

    Commit Init with STS. Close #1

---
+++ ApplicationSUser/src/main/java/com/nest/appuser/ApplicationSUserApplication.java
@@ -0,0 +1,13 @@
+package com.nest.appuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationSUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationSUserApplication.class, args);
+	}
+
+}
---
+++ ApplicationSUser/src/test/java/com/nest/appuser/ApplicationSUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.appuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationSUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
